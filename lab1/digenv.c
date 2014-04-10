b15bbe894f0e4dcfe40d154ee4f4ee194e0a90b4
Shyhwang Ong <shyhwang@csc.kth.se>
2014-04-10 14:39:07 +0200

    inga dubbletter

--- lab1/digenv.c
+++ lab1/digenv.c
@@ -13,12 +13,11 @@
 #define FALSE ( 0 ) /* define the bool constant FALSE*/
 
 pid_t childpid; /* för child-processens PID vid fork() */
-pid_t childpid2; /* sort */
-pid_t childpid3; /* less */
-
+pid_t childpid2;
+pid_t childpid3;
 int main(int argc, char **argv, char **envp)
 {	
-	 fprintf( stderr, "Parent (Parent, pid %ld) started\n",
+	fprintf( stderr, "Parent (Parent, pid %ld) started\n",
            (long int) getpid() );
 
 	printf("argc: %d\n", argc);
@@ -32,17 +31,16 @@ int main(int argc, char **argv, char **envp)
 
 	returnValue = pipe( pipe_fileDesc ); /* Create a pipe */
 	if ( -1 == returnValue) { perror("Cannot create pipe");	exit(1); }
+
 	childpid = fork(); /* Create the child process for printenv */ 
-
-	returnValue = pipe( pipe_fileDesc2 );
-	if ( -1 == returnValue) { perror("Cannot create pipe");	exit(1); }
-	childpid2 = fork(); 
-
-	/*
-	returnValue = pipe( pipe_fileDesc2 );
-	if ( -1 == returnValue) { perror("Cannot create pipe");	exit(1); } */
-	childpid3 = fork(); 
-	
+	if (-1 == childpid) /* Fork failed*/
+		{
+			char * errorMessage = "UNKNOWN";
+			if (EAGAIN == errno){errorMessage = "cannot allocate page table";}
+			if (ENOMEM == errno){errorMessage = "cannot allocate kernel data";}
+			fprintf(stderr, "fork() blew up because: %s\n", errorMessage);
+			exit(1);
+		}
 	/* ---------------------------- Child printEnv ---------------------------- */
 	if (0 == childpid)
 	{
@@ -60,7 +58,21 @@ int main(int argc, char **argv, char **envp)
 		perror("Cannot exec printenv");
 		exit(1);
 	}
+	returnValue = close(pipe_fileDesc[PIPE_WRITE_SIDE]);
+       	if (-1 == returnValue)	{ perror("Cannot close pipe");	exit(1); }
 
+	returnValue = pipe( pipe_fileDesc2 );
+	if ( -1 == returnValue) { perror("Cannot create pipe");	exit(1); }
+
+	childpid2 = fork();
+	if (-1 == childpid2) /* Fork failed*/
+		{
+			char * errorMessage = "UNKNOWN";
+			if (EAGAIN == errno){errorMessage = "cannot allocate page table";}
+			if (ENOMEM == errno){errorMessage = "cannot allocate kernel data";}
+			fprintf(stderr, "fork() blew up because: %s\n", errorMessage);
+			exit(1);
+		}
 	/* ---------------------------- Child sort ---------------------------- */
 	else if (0 == childpid2)
 	{
@@ -70,20 +82,30 @@ int main(int argc, char **argv, char **envp)
 		returnValue = dup2(pipe_fileDesc[PIPE_READ_SIDE], STDIN_FILENO);
 		returnValue = dup2(pipe_fileDesc2[PIPE_WRITE_SIDE], STDOUT_FILENO);
 
-		/* Close the read end of this pipe, child doesn't have to write data*/
-		returnValue = close(pipe_fileDesc[PIPE_WRITE_SIDE]);
-		if (-1 == returnValue)
-		{ perror("Cannot close pipe");	exit(1); }
-
 		returnValue = close(pipe_fileDesc2[PIPE_READ_SIDE]);
-		if (-1 == returnValue)
-		{ perror("Cannot close pipe");	exit(1); }
+		if (-1 == returnValue)	{ perror("Cannot close pipe");	exit(1); }
 
 		(void) execlp("sort", "sort", (char *) 0);
 		perror("Cannot exec printenv");
 		exit(1);
 	}
+	returnValue = close(pipe_fileDesc[PIPE_READ_SIDE]);
+	if (-1 == returnValue)	{ perror("Cannot close pipe");	exit(1); }
+	returnValue = close(pipe_fileDesc2[PIPE_WRITE_SIDE]);
+	if (-1 == returnValue)	{ perror("Cannot close pipe");	exit(1); }
 
+	/*
+	returnValue = pipe( pipe_fileDesc2 );
+	if ( -1 == returnValue) { perror("Cannot create pipe");	exit(1); } */
+	childpid3 = fork(); 
+	if (-1 == childpid3) /* Fork failed*/
+		{
+			char * errorMessage = "UNKNOWN";
+			if (EAGAIN == errno){errorMessage = "cannot allocate page table";}
+			if (ENOMEM == errno){errorMessage = "cannot allocate kernel data";}
+			fprintf(stderr, "fork() blew up because: %s\n", errorMessage);
+			exit(1);
+		}
 	/* ---------------------------- Child less ---------------------------- */
 	else if (0 == childpid3)
 	{
@@ -92,72 +114,18 @@ int main(int argc, char **argv, char **envp)
 
 		returnValue = dup2(pipe_fileDesc2[PIPE_READ_SIDE], STDIN_FILENO);
 
-		/* Close the read end of this pipe, child doesn't have to write data*/
-		returnValue = close(pipe_fileDesc2[PIPE_WRITE_SIDE]);
-		if (-1 == returnValue)
-		{ perror("Cannot close pipe");	exit(1); }
-
 		(void) execlp("less", "less", (char *) 0);
 		perror("Cannot exec printenv");
 		exit(1);
 	}
+	returnValue = close(pipe_fileDesc2[PIPE_READ_SIDE]);
+       	if (-1 == returnValue)	{ perror("Cannot close pipe");	exit(1); }
+
 /* ---------------------------- Parent ---------------------------- */
 	else{
- 		if (-1 == childpid) /* Fork failed*/
-		{
-			char * errorMessage = "UNKNOWN";
-
-			if (EAGAIN == errno)
-			{
-				errorMessage = "cannot allocate page table";
-			}
-
-			if (ENOMEM == errno)
-			{
-				errorMessage = "cannot allocate kernel data";
-			}
-
-			fprintf(stderr, "fork() blew up because: %s\n", errorMessage);
-			exit(1);
-		}
-
-		if (-1 == childpid2) /* Fork failed*/
-		{
-			char * errorMessage = "UNKNOWN";
-
-			if (EAGAIN == errno)
-			{
-				errorMessage = "cannot allocate page table";
-			}
-
-			if (ENOMEM == errno)
-			{
-				errorMessage = "cannot allocate kernel data";
-			}
-
-			fprintf(stderr, "fork() blew up because: %s\n", errorMessage);
-			exit(1);
-		}
-
-		if (-1 == childpid3) /* Fork failed*/
-		{
-			char * errorMessage = "UNKNOWN";
-
-			if (EAGAIN == errno)
-			{
-				errorMessage = "cannot allocate page table";
-			}
-
-			if (ENOMEM == errno)
-			{
-				errorMessage = "cannot allocate kernel data";
-			}
-
-			fprintf(stderr, "fork() blew up because: %s\n", errorMessage);
-			exit(1);
-		}
+ 	
 /* ---------------------------- Parent code starts here ---------------------------- */
-
+	printf("Parent here\n");
 		/* TODO Handle signals */
 	}
 	exit(0); // Nomral terminate
