e1afb414e11a983207343266314162dc28b69e33
Perka <perka@Mac.com>
2014-04-10 11:12:31 +0200

    Moving on to pagin
    TODO: We have  a bug where by we only print the first 8 chars of each enviorment var, mail asse!

--- lab1/digenv.c
+++ lab1/digenv.c
@@ -36,7 +36,6 @@ int main(int argc, char **argv, char **envp)
 	childpid = fork(); /* Create the child process for printenv */ 
 	if (0 == childpid)
 	{
-
 		fprintf( stderr, "Child (printenv, pid %ld) started\n",	
              (long int) getpid() );
 
@@ -53,7 +52,6 @@ int main(int argc, char **argv, char **envp)
 		if (argc == 1) /* Directly to sort */
 		{
 			(void) execlp("sort", "sort", (char *) 0);
-
 			perror("Cannot exec sort");
 			exit(1);
 		}
@@ -103,25 +101,23 @@ int main(int argc, char **argv, char **envp)
 			exit(1);
 		}
 
-		printf("Size of envp: %d\n", sizeof(envp));
-
-
-		returnValue = write( pipe_fileDesc[PIPE_WRITE_SIDE], *&envp[0], sizeof(envp)); //sort gets wrong sizeof and causes write without newline
-
-			if (-1 == returnValue)
-			{
-				perror("Parent cannot write to pipe");
-				exit(1);
-			}
-		
-
-		/*int i;
+		int i;
 		for (i = 0; envp[i] != NULL; ++i)
 		{
-		
-		//printf("%2d:%s\n", i , envp[i]);
-		//printf("%s\n", envp[i]);
-		} */
+			char newLine = '\n';
+			//int test = sizeof(**envp[i]);
+			/* printf("DEBUG: %2d:%s\n", i , envp[i]); */
+			returnValue = write(pipe_fileDesc[PIPE_WRITE_SIDE], *&envp[i], sizeof(envp[i]));
+			//printf("DEBUG: Size of envp: %d\n", **envp[i]);
+			returnValue = write(pipe_fileDesc[PIPE_WRITE_SIDE], &newLine, sizeof(newLine));
+
+			if (-1 == returnValue)
+				{
+					perror("Parent cannot write to pipe");
+					exit(1);
+				}
+			//printf("%s\n", envp[i]);
+		} 
 	}
 	exit(0); // Nomral terminate
 }
