b52a3d8d6802ce401c59e6bed54fcb5411fac067
Perka <perka@Mac.com>
2014-04-28 11:53:26 +0200

    CODU SENSAI

--- lab2/spshell.c
+++ lab2/spshell.c
@@ -8,11 +8,20 @@
 
 pid_t childpid; /* childProcessID for printenvp */
 int returnValue; /* Return value, to findout if an error occured */
-int status; /* Return codes for children */ 
+int status; /* Return codes for children */
+
+#define BUFFERSIZE (81) /* Define maximum size of the buffer, assumption from lab specification */
+#define ARGVSIZE (6) /* Define maximum size of the ARGC, assumption from lab specification */
 
 int main(int argc, char **argv)
 {	
+	fprintf( stderr, "Parent (Parent, pid %ld) started\n", (long int) getpid() );  /* printing parents id for easier debuggning*/
 
+	char inputBuffer[BUFFERSIZE]; /* Declare input buffer for command, hard code to Stack */
+
+	fgets(inputBuffer, BUFFERSIZE , stdin); /* Read command from terminal */
+
+	printf("DEBUG: input: %s\n", inputBuffer);
 
 	exit(0); // Normal terminate
 }
