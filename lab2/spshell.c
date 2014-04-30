c0876eee5c27903e0f41f252666a10465e5a04b8
Perka <perka@Mac.com>
2014-04-30 10:41:58 +0200

    Fixed the ls -la bug, waitpid

--- lab2/spshell.c
+++ lab2/spshell.c
@@ -18,8 +18,6 @@ int status; /* Return codes for children */
 #define BUFFERSIZE (81) /* Define maximum size of the buffer, assumption from lab specification */
 #define ARGVSIZE (6) /* Define maximum size of the ARGC, assumption from lab specification */
 
-
-
 /* createChild will create a child that will execute a command and change STDOUT to be sent to the write pipe and STDIN sent to read pipe.
 @Param	**input	- Input arguments to be run, that has been feed into the shell. */
 void createChild(char **input)
@@ -45,7 +43,7 @@ void createChild(char **input)
 we need to handle the child when it dies or it will become a zombie */
 void childHandler()
 {
-	childpid = wait( &status ); /* waiting on a child process */
+	waitpid( childpid , &status , 0 ); /* waiting on a child process */
 	if ( -1 == childpid) { perror( "wait() failed unexpectedly" ); exit( 1 ); }/* Check if wait works correctly*/
 
  	if( WIFEXITED( status ) ) /* Check if child has terminated normally or by signal */
@@ -73,16 +71,15 @@ int main(int argc, char **argv)
 {	
 	fprintf( stderr, "Parent (Parent, pid %ld) started\n", (long int) getpid() );  /* printing parents id for easier debuggning*/
 
-	char inputBuffer[BUFFERSIZE]; /* Declare input buffer for command, hard code to Stack */
-	char **arguments = (char **) calloc (ARGVSIZE,  BUFFERSIZE); /* Allocate memory for toknizer */
-	
-	double timeElapsed; /* Declare a time elapsed varible */
-
 	while(1){ /* Loop forever */
 		printf("DEBUG: Start of loop\n");
+		char inputBuffer[BUFFERSIZE]; /* Declare input buffer for command, hard code to Stack */
+		char **arguments = (char **) calloc (ARGVSIZE,  BUFFERSIZE); /* Allocate memory for toknizer */
+		
+		double timeElapsed; /* Declare a time elapsed varible */
+
 		fgets(inputBuffer, BUFFERSIZE , stdin); /* Read command from terminal */
 		inputBuffer[strlen(inputBuffer)-1]= '\0'; /* Remove the newline char and replace it with null */ 
-		
 		char *arg = strtok(inputBuffer, " "); /* Split the input on space */
 		
 		/* Incase of spaces or enter*/
@@ -124,10 +121,10 @@ int main(int argc, char **argv)
 		}
 		
 		/* Create the struct for our time variables according to the man page*/
-		struct timeval starTime ,endTime;
+		struct timeval starTime , endTime;
 		gettimeofday(&starTime, NULL); /* Get start time */
 		createChild(arguments);
-		childHandler();
+		childHandler(); /* Send PID */
 		gettimeofday(&endTime, NULL); /* Get end time*/
 
 		/* Calculate and then convert to micro seconds */
@@ -136,9 +133,9 @@ int main(int argc, char **argv)
 
 		printf("Wallclock time: %f msec \n", timeElapsed); 
 
+		free(arguments); /* Relese memory */
 		printf("DEBUG: End of loop\n");
 	}
 
-	free(arguments); /* Relese memory */
 	exit(0);
 }
