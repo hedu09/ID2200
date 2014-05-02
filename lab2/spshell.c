5414b885c400b9c8e9237dd4fcd3a1d3e86558aa
Shyhwang Ong <shyhwang@csc.kth.se>
2014-05-02 16:08:18 +0200

    done done done

--- lab2/spshell.c
+++ lab2/spshell.c
@@ -9,10 +9,10 @@
 #include <unistd.h> /* Add the cd command */
 #include <sys/time.h> /* Get timeofday() function */
 
-
 /* Function declarations */
 void createChild (char**);
 void childHandler();
+void donothinghandler(int s);
 
 pid_t childpid; /* childProcessID for printenvp */
 int status; /* Return codes for children */
@@ -24,19 +24,16 @@ int status; /* Return codes for children */
 
 /* createChild will create a child that will execute a command and change STDOUT to be sent to the write pipe and STDIN sent to read pipe.
 @Param	**input	- Input arguments to be run, that has been feed into the shell. */
-void createChild(char **input)
-{
+void createChild(char **input) {
 	childpid = fork(); /* Create a new process */
-	if (-1 == childpid)
-	{
+	if (-1 == childpid) {
 		char * errorMessage = "UNKNOWN"; /* if no known error message print UNKNOWN*/
 		if (EAGAIN == errno){errorMessage = "cannot allocate page table";}
 		if (ENOMEM == errno){errorMessage = "cannot allocate kernel data";}
 		fprintf(stderr, "fork() blew up because: %s\n", errorMessage);
 		exit(1);/* Exit with error*/
 	}
-	else if(0 == childpid)/* Child returns 0 */
-	{
+	else if(0 == childpid) { /* Child returns 0 */
 		fprintf( stderr, "Child (%s, pid %ld) started\n", input[0], (long int) getpid() );
 		(void) execvp(input[0], input); /* Execute command */
 		perror("Cannot exec perror");
@@ -46,24 +43,21 @@ void createChild(char **input)
 /* Handles the child when the child has terminated by signal or died with or without error
 we need to handle the child when it dies or it will become a zombie */
 /* @Param	wait - Process will wait until it recieve a signal. */
-void childHandler(int wait)
-{
+void childHandler(int wait) {
 	if(wait == WAIT){  /* Process will wait until it recieve a signal */
 		waitpid( childpid , &status , 0 ); /* waiting on a child process */		
 		if ( -1 == childpid) { perror( "wait() failed unexpectedly" ); exit( 1 ); } /* Check if wait works correctly*/
 
-		if( WIFEXITED( status )  ) { /* Check if child has terminated normally or by signal */
+		if( WIFEXITED( status )) { /* Check if child has terminated normally or by signal */
 			int child_status = WEXITSTATUS( status ); /*get the exit code specified by the child process*/
-			if( 0 != child_status ) /* child had problems */
-			{
+			if( 0 != child_status ) {/* child had problems */
 				fprintf( stderr, "Child (pid %ld) failed with exit code %d\n",
 					(long int) childpid, child_status );
 			}
 			printf("Foreground process: (pid %ld), terminated\n", (long int) childpid); 
 		}
 		else {
-			if( WIFSIGNALED( status ) ) /* child-process terminated by signal */
-			{
+			if( WIFSIGNALED( status ) ) {/* child-process terminated by signal */
 				int child_signal = WTERMSIG( status ); /*get the exit code specified by the child process*/
 				fprintf( stderr, "Child (pid %ld) was terminated by signal no. %d\n",
 					(long int) childpid, child_signal );
@@ -75,16 +69,14 @@ void childHandler(int wait)
 		while((returnvalue = waitpid(-1, &status ,WNOHANG)) > 0){			
 			if( WIFEXITED( status )  ) { /* Check if child has terminated normally or by signal */
 				int child_status = WEXITSTATUS( status ); /*get the exit code specified by the child process*/
-				if( 0 != child_status ) /* child had problems */
-				{
+				if( 0 != child_status ) {/* child had problems */
 					fprintf( stderr, "Child (pid %ld) failed with exit code %d\n",
 						(long int) childpid, child_status );
 				}
 				printf("Background process: (pid %ld), terminated\n", (long int) childpid); 
 			}
 			else {
-				if( WIFSIGNALED( status ) ) /* child-process terminated by signal */
-				{
+				if( WIFSIGNALED( status ) ) { /* child-process terminated by signal */
 					int child_signal = WTERMSIG( status ); /*get the exit code specified by the child process*/
 					fprintf( stderr, "Child (pid %ld) was terminated by signal no. %d\n",
 						(long int) childpid, child_signal );
@@ -93,6 +85,13 @@ void childHandler(int wait)
 		}		
 	}
 }
+/* This signal handler does absolutly nothing, due to spefications. 
+@ Param int s - type of signal 
+*/
+void donothinghandler(int s){
+        /*   printf("Caught signal %d\n and ignore it \n",s); */
+}
+
 /* Main acts as the parents process in this program, the program starts with entering an infintate loop in order to capture commands given by the user.
 This is done by a string tokenizer that uses diffrent if cases to detect and sanatize input. A user can change directory and give the exit command.
 Programs may be started as foreground or background processes with the use of the '&' sign. When starting a foreground process some statistics is 
@@ -100,11 +99,13 @@ presented for the user and then the loop starts again.
 @ Param int argc - number of arguments we have started our program with 
 @ Param char **argv - all the arguments we have is in a vector 
 */
-int main(int argc, char **argv)
-{	
+int main(int argc, char **argv) {	
 	fprintf( stderr, "Parent (Parent, pid %ld) started\n", (long int) getpid() );  /* printing parents id for easier debuggning*/
-	sigignore(SIGINT); /* Ignore Ctrl + C from user */
-	/*signal(SIGINT, ignore);*/
+	
+	struct sigaction sa; /* Create a struct for sa */
+	sa.sa_handler = donothinghandler; /* Set the handler to our donothinghandler */
+	sa.sa_flags = SA_RESTART; /* Restart function if interrupted by handler */
+	sigaction(SIGINT, &sa, NULL); /* Call sighandler with interupt signal */
 
 	while(1){ /* Loop forever */
 		printf("DEBUG: Start of loop\n");		
@@ -120,14 +121,12 @@ int main(int argc, char **argv)
 		char *arg = strtok(inputBuffer, " "); /* Split the input on space */
 		
 		/* Incase of spaces or enter*/
-		if (arg == NULL)
-		{
+		if (arg == NULL) {
 			printf("Say again?\n");
 			continue; /* Reloop */
 		}
 
-		if (strcmp( "exit", arg) == 0) /* Exit command was given */
-		{
+		if (strcmp( "exit", arg) == 0) {/* Exit command was given */
 			free(arguments); /* Relese memory */
 			printf("Thank you come again!\n");
 			exit(0); /* Terminate the program normaly due to exit */
@@ -141,17 +140,14 @@ int main(int argc, char **argv)
 			length++;
 		}
 		
-		if (strcmp( "cd", arguments[0]) == 0) /* check if first argument is cd */
-		{
-			if (-1 == chdir(arguments[1])) /* Something went wrong */
-			{
+		if (strcmp( "cd", arguments[0]) == 0) {/* check if first argument is cd */
+			if (-1 == chdir(arguments[1])) {/* Something went wrong */
 				char *homeEnv; /* char pointer for the home varible */
 				homeEnv = getenv("HOME"); /* Get the home */
 				printf("DEBUG: Selected home: %s\n", homeEnv);
 				chdir(homeEnv); /* Fallback */
 			}
-			else /* chdir returned 0 */
-			{
+			else { /* chdir returned 0 */
 				chdir(arguments[1]); 
 			}
 			free(arguments); /* Relese memory */
